e5cc99c64dde5206383fd006c5d5b22de69b751d
fouchimi <ousmanefouchimi@gmail.com>
2025-12-25 13:55:28 -0500

    Added hello.py

---
+++ cs50/hello.py
@@ -0,0 +1,12 @@
+print('Hello World!')
+x = int(input("Type a number: "))
+
+if x > 0:
+    print("x is positive")
+elif x < 0:
+    print("x is negative")
+else:
+    print("x is zero")
+
+name = "John"
+print(name[0]);
