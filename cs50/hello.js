2eb0dd3a08e1c8d9d2a355c9eaaf59898aa32be9
fouchimi <ousmanefouchimi@gmail.com>
2026-01-08 16:16:12 -0500

    Added hello.html, hello.js, counter.html and counter.js

--- cs50/hello.js
+++ cs50/hello.js
@@ -1,5 +1,6 @@
 function hello() {
-    const age = 30;
-    alert("I am " + age + " years old");
-    document.querySelector("h1").innerHTML = "Hello!";
+    const headings = document.querySelectorAll("h1");
+    for (const heading of headings) {
+        heading.innerHTML = "Hello!"
+    }
 }
